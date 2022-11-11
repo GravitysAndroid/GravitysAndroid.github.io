43615a3dc50af6367593389d4b8e697553eddc2f
Hayden Williams <haydenwill95@gmail.com>
2022-11-11 14:10:02 +1300

    Added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,36 @@ import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+	return (
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Call</LinkTitle>
+					<LinkItem href='tel:+64272133801'>+64272133801</LinkItem>
+				</LinkColumn>
+				<LinkColumn>
+					<LinkTitle>Email</LinkTitle>
+					<LinkItem href='mailto:haydenwill95@gmail.com'>haydenwill95@gmail</LinkItem>
+				</LinkColumn>
+			</LinkList>
+			<SocialIconsContainer>
+				<CompanyContainer>
+					<Slogan>Innovating one project at a time</Slogan>
+				</CompanyContainer>
+				<SocialContainer>
+					<SocialIcons href='https://github.com'>
+						<AiFillGithub size='3rem' />
+					</SocialIcons>
+					<SocialIcons href='https://linkedin.com'>
+						<AiFillLinkedin size='3rem' />
+					</SocialIcons>
+					<SocialIcons href='https://instagram.com'>
+						<AiFillInstagram size='3rem' />
+					</SocialIcons>
+				</SocialContainer>
+			</SocialIconsContainer>
+		</FooterWrapper>
+	);
 };
 
 export default Footer;
